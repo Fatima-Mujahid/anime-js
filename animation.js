de6c10ae3dec5752b81abd6db04cba3e3eb59158
Fatima-Mujahid <fmujahid.bese19seecs@seecs.edu.pk>
2023-02-13 21:09:21 +0500

    animated shapes

--- animation.js
+++ animation.js
@@ -1,10 +1,50 @@
 import anime from "animejs";
 
-export function animation(element) {
-    anime({
-        targets: element,
-        position: 300
-    })
-}
+const squares = document.getElementsByClassName("square");
+const allElements = document.querySelectorAll("div");
 
-console.log("Hi")
+// export function animation(element) {
+//     anime({
+//         targets: element,
+//         left: 300
+//     })
+// }
+
+// anime({
+//   targets: allElements,
+//   left: 300,
+//   opacity: 0.5,
+//   height: 50,
+//   borderRadius: 0,
+// });
+
+// anime({
+//   targets: allElements,
+//   left: 300,
+//   rotate: 50,
+//   scale: 0.5,
+//   translateY: 100,
+//   opacity: 0.5,
+// });
+
+// allElements.forEach((element, index) => {
+//   anime({
+//     targets: element,
+//     left: 300 + index * 50,
+//   });
+// });
+
+anime({
+  targets: allElements,
+  left: 300,
+});
+
+anime({
+  targets: squares,
+  borderRadius: 50,
+});
+
+anime({
+  targets: ".circle",
+  borderRadius: 0,
+});
